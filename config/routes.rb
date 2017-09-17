56159e04ff21587d7a9bab3683fc02b823903411
raptorggi <raptorggi@gmail.com>
2017-09-17 15:20:46 +1000

    feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'about' => 'application#about'
-
-  get 'welcome/index'
-  root 'welcome#index'
+  get 'index' => 'application#index'
+  get 'feedback/new'
+  post 'feedback' => 'feedback#create'
+  root 'application#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
