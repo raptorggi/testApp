b971087a84228e9c1b18ea8210039b2adcd65642
raptorggi <raptorggi@gmail.com>
2017-10-21 23:10:19 +1000

    add rspec

---
+++ spec/controllers/feedback_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe FeedbacksController, type: :controller do
+  describe "new" do
+    it "response successfully" do
+      get :new
+      expect(response).to be_success
+    end
+
+    it "feedback be a new" do
+      get :new
+      expect(assigns(:feedback)).to be_a_new(Feedback)
+    end
+
+    it "feedbacks be a new" do
+      create_list(:feedback, 5, name: '1', email: '2', phone: '3', message: '4')
+      get :new
+      expect(assigns(:feedbacks).count).to eq(5) 
+    end
+  end
+end
---
+++ spec/factories/feedback.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :feedback do
+    name "John"
+    email  "Doe"
+    phone "sdfsdf"
+    message "sdfsdfs"
+  end
+end
