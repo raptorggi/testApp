71ec0642f3bee4abc7739c9a1032ae3ea11dff7d
raptorggi <raptorggi@gmail.com>
2017-10-21 23:10:19 +1000

    add rspec

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ gem 'bootstrap-sass', '~> 3.3.6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  gem 'rails-controller-testing'
+  gem "factory_bot_rails", "~> 4.0"
+  gem 'shoulda-matchers', '~> 3.1'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
---
+++ spec/models/feedback_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Feedback do
+  describe "presence" do
+    it "name" do
+      feedback = Feedback.create(email: "123", phone: "123", message: "3213")
+      #byebug
+      expect(feedback.errors.any?).to be_truthy
+    end
+
+    it "name" do
+      feedback = Feedback.create(email: "123", phone: "123", message: "3213")
+      expect(feedback.errors.messages).to include(:name)
+    end
+
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :phone }
+    it { is_expected.to validate_presence_of :message }
+  end
+end
