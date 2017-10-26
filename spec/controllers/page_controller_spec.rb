3be664e76f017f10f9044ffddde522c3e07d72f3
raptorggi <raptorggi@gmail.com>
2017-10-26 22:40:03 +1000

    dynamic pages fix 2

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 describe PageController, type: :controller do
   describe "show" do
-    it "responce successfully" do
+    it "render template" do
       create :page, slug: 'page-1'
       get :show, params: {slug: 'page-1'}
       expect(response).to render_template("show")
     end 
+    it "returns a 200 status code" do 
+      create :page, slug: 'page-1'
+      get :show, params: {slug: 'page-1'}
+      expect(response).to have_http_status(200)
+    end
+    it "returns requested page" do
+      p = create :page, slug: 'page-1'
+      get :show, params: {slug: 'page-1'}
+      expect(assigns(:page).id).to eq(p.id) 
+    end
   end
 end
