59f0b30a6993809e17a1b48aafeb67e352fd550b
Unknown <raptorggi@gmail.com>
2018-08-21 16:39:29 +1000

    added products and category caching

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  caches_action :index, :tag => 'products_index'#, :layout => false
-  caches_action :category, :tag => 'products_category'#, :layout => false
-  caches_action :show, :tag => 'products_show'#, :layout => false
+
+  caches_action :index, :tag => 'products_index'
+  caches_action :category, :tag => 'products_category'
+  caches_action :show, :tag => 'products_show'
+  
   cache_sweeper :products_sweeper, :only => [ :category, :show ]
   cache_sweeper :category_sweeper, :only => [ :category, :index ]
 
