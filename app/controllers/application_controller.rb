f1babf46c60b89e9426a7a38b82481d180ca3255
Konstantin Denisov <raptorggi@gmail.com>
2018-08-29 17:55:23 +1000

    opened seed for production and fixed routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
     user_signed_in?
   end
 
-  private 
-  
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
     I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
